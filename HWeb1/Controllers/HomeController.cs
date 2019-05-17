39bf87d18f2bae530872c277952feb485c5f6a3e
petriucmihai <mihaitp10@gmail.com>
2019-05-16 17:24:20 -0700

    Hooked up the Model to Home and Index View

--- HWeb1/Controllers/HomeController.cs
+++ HWeb1/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace HWeb1.Controllers
                 LastAcessDate = DateTime.Now
             };
 
-            return View();
+            return View(myData);
         }
 
         public ActionResult FAQ()
